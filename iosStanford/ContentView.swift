d55938dff398870cd6aaaf45df51c3b4d2f550f5
Jan Sandau <j.sandau@me.com>
2020-07-29 22:01:16 +0200

    Initial Commit

---
+++ iosStanford/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosStanford
+//
+//  Created by Jan Sandau on 29.07.20.
+//  Copyright © 2020 Jan Sandau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
