fb1f6e156c38dcb5318b2750a9d90482b1f981f9
Jan <j.sandau@me.com>
2020-07-29 22:10:16 +0200

    init

--- iosStanford/ContentView.swift
+++ iosStanford/ContentView.swift
@@ -6,16 +6,27 @@
 //  Copyright © 2020 Jan Sandau. All rights reserved.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            RoundedRectangle(cornerRadius: 10.0).fill()
+            Text("😬").font(Font.largeTitle)
+        })
+            .padding()
+            .foregroundColor(Color.orange)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
+
